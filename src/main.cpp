aac26ab094bf9dd487ca655d1f112c271772a3c7
EricLBuehler <ericlbuehler@gmail.com>
2024-02-16 16:00:54 -0500

    Cleaner code

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 namespace stdx = std::experimental;
 
-template <typename _Tp, int R, int C>
+template <typename _Tp, size_t R, size_t C>
 class SlowMatrix {
    public:
     std::array<std::array<_Tp, C>, R>* data;
@@ -48,7 +48,7 @@ class SlowMatrix {
         }
     }
 
-    template <int _R, int _C>
+    template <size_t _R, size_t _C>
     static SlowMatrix<_Tp, _C, _R> transpose(
         const SlowMatrix<_Tp, _R, _C>& matrix) {
         // Do the transpose
@@ -67,13 +67,13 @@ class SlowMatrix {
         return Matrix<_Tp, _C, _R>(transpose);
     }
 
-    template <int R2>
+    template <size_t R2>
     SlowMatrix<_Tp, R, R2> matmul(const SlowMatrix<_Tp, C, R2>& other) {
         const SlowMatrix<_Tp, R2, C> other_T = transpose(other);
         return this->matmul_pre_T(other_T);
     }
 
-    template <int R2>
+    template <size_t R2>
     SlowMatrix<_Tp, R, R2> matmul_pre_T(const SlowMatrix<_Tp, R2, C>& other_T) {
         std::array<std::array<_Tp, R2>, R>* arr =
             new std::array<std::array<_Tp, R2>, R>{};
@@ -97,7 +97,7 @@ class SlowMatrix {
     ~SlowMatrix() { delete this->data; }
 };
 
-template <typename _Tp, int R, int C>
+template <typename _Tp, size_t R, size_t C>
 class Matrix {
    public:
     std::array<stdx::fixed_size_simd<_Tp, C>, R>* data;
@@ -115,22 +115,15 @@ class Matrix {
         this->data = arr;
     }
 
-    template <int R2>
-    SlowMatrix<_Tp, R, R2> matmul_pre_T(const Matrix<_Tp, R2, C>& other_T) {
-        std::array<std::array<_Tp, R2>, R>* arr =
-            new std::array<std::array<_Tp, R2>, R>{};
-
-        for (int r1 = 0; r1 < R; r1++) {       // R of ours
-            for (int r2 = 0; r2 < R2; r2++) {  // R of other
-                // Do the mul here
-                stdx::fixed_size_simd<_Tp, C> tmp =
-                    (*this->data)[r1] * (*other_T.data)[r2];
-
-                (*arr)[r1][r2] =
-                    stdx::parallelism_v2::reduce(tmp, std::plus<>());
+    template <size_t R2>
+    void matmul_pre_T(const Matrix<_Tp, R2, C>& other_T,
+                      std::array<std::array<_Tp, R2>, R>* out) {
+        for (size_t r1 = 0; r1 < R; r1++) {       // R of ours
+            for (size_t r2 = 0; r2 < R2; r2++) {  // R of other
+                (*out)[r1][r2] = stdx::parallelism_v2::reduce(
+                    (*this->data)[r1] * (*other_T.data)[r2], std::plus<>());
             }
         }
-        return SlowMatrix<_Tp, R, R2>(arr);
     }
 
     void print() const {
@@ -146,41 +139,40 @@ class Matrix {
 };
 
 template <size_t R, size_t C, size_t K>
-std::array<std::array<int, K>, R> multiplyMatrices(
-    const std::array<std::array<int, C>, R>* mat1,
-    const std::array<std::array<int, K>, C>* mat2) {
-    std::array<std::array<int, K>, R> result;
-
+void multiplyMatrices(const std::array<std::array<int, C>, R>* mat1,
+                      const std::array<std::array<int, K>, C>* mat2,
+                      std::array<std::array<int, K>, R>* out) {
     for (size_t i = 0; i < R; ++i) {
         for (size_t j = 0; j < K; ++j) {
             int sum = 0;
             for (size_t k = 0; k < C; ++k) {
                 sum += (*mat1)[i][k] * (*mat2)[k][j];
             }
-            result[i][j] = sum;
+            (*out)[i][j] = sum;
         }
     }
-
-    return result;
 }
 
 int main(int argc, char** argv) {
     constexpr int ROWS = 10;
     constexpr int COLS = 10;
     constexpr int WIDTH = 16;
-    constexpr int TIMES = 10;
+    constexpr int TIMES = 1;
 
     SlowMatrix<int, ROWS, WIDTH> a_slow;
     SlowMatrix<int, COLS, WIDTH> b_slow;
     Matrix<int, ROWS, WIDTH> a(a_slow);
     Matrix<int, COLS, WIDTH> b(b_slow);
 
+    std::array<std::array<int, ROWS>, COLS>* arr =
+        new std::array<std::array<int, ROWS>, COLS>{};
+
     int64_t total_time = 0;
     for (int i = 0; i < TIMES; i++) {
         auto start = std::chrono::high_resolution_clock::now();
 
-        for (int j = 0; j < 100; j++) {
-            a.matmul_pre_T(b);
+        for (int j = 0; j < 1000000; j++) {
+            a.matmul_pre_T(b, arr);
         }
 
         auto end = std::chrono::high_resolution_clock::now();
@@ -200,8 +192,8 @@ int main(int argc, char** argv) {
     for (int i = 0; i < TIMES; i++) {
         auto start2 = std::chrono::high_resolution_clock::now();
 
-        for (int j = 0; j < 100; j++) {
-            multiplyMatrices(mat1, mat2);
+        for (int j = 0; j < 1000000; j++) {
+            multiplyMatrices(mat1, mat2, arr);
         }
 
         auto end2 = std::chrono::high_resolution_clock::now();
